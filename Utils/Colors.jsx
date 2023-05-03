3d9fd266537b74432eb7b4329f5bc5b2996f1813
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-05-03 23:55:48 +0200

    Colors added
    Folders have been added and Colors have been added as well.

---
+++ Utils/Colors.jsx
@@ -0,0 +1,11 @@
+export const Colors = {
+    primary: '#282C30',
+    secondary: '#F4405D',
+    secondary600: '#D9003D',
+    tertiary: '#FB662D',
+    tertiary600: '#E25A24',
+    Success: '#17AFA7',
+    Success600: '#127C77',
+    Danger: '#FD0C32',
+    White: '#F7F7F7'
+};
