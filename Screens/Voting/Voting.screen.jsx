124186653a4e4bcd0adb00bddf5aea47c829615e
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-11-15 02:27:28 +0200

    Revert "push notifications working"
    This reverts commit d40f7833f6937d652b17883a98ebe1392176b9a7.

--- Screens/Voting/Voting.screen.jsx
+++ Screens/Voting/Voting.screen.jsx
@@ -22,10 +22,10 @@ import {
 import { useFocusEffect, useIsFocused } from "@react-navigation/native";
 import { TextStyles } from "../../Utils/Text";
 import { FirebaseContext } from "../../store/FirebaseUser.context";
-// import {
-//   registerForPushNotificationsAsync,
-//   sendPushNotification,
-// } from "../../store/pushNotifications";
+import {
+  registerForPushNotificationsAsync,
+  sendPushNotification,
+} from "../../store/pushNotifications";
 import * as Notifications from "expo-notifications";
 
 export default function Voting({ route, navigation }) {
@@ -53,31 +53,27 @@ export default function Voting({ route, navigation }) {
   const notificationListener = useRef();
   const responseListener = useRef();
 
-  // useEffect(() => {
-  //     try {
-  //       registerForPushNotificationsAsync().then((token) => setPushNotToken(token));
-  //       notificationListener.current =
-  //         Notifications.addNotificationReceivedListener((notification) => {
-  //           setNotification(notification);
-    
-  //           responseListener.current =
-  //             Notifications.addNotificationResponseReceivedListener((response) => {
-  //               console.log(response);
-  //             });
-    
-  //           return () => {
-  //             Notifications.removeNotificationSubscription(
-  //               notificationListener.current
-  //             );
-  //             Notifications.removeNotificationSubscription(
-  //               responseListener.current
-  //             );
-  //           };
-  //         });
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
-  // }, []);
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => setPushNotToken(token));
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+
+        responseListener.current =
+          Notifications.addNotificationResponseReceivedListener((response) => {
+            console.log(response);
+          });
+
+        return () => {
+          Notifications.removeNotificationSubscription(
+            notificationListener.current
+          );
+          Notifications.removeNotificationSubscription(
+            responseListener.current
+          );
+        };
+      });
+  }, []);
 
   const [index, setIndex] = useState(0);
 
